f12990842c8f1d6eeee51958c1635342456505c1
simonmtr <294simon@googlemail.com>
2023-01-02 10:44:56 +0100

    initial commit with backend

---
+++ backend/springangularlogin/src/main/java/dev/springangularlogin/springangularlogin/SpringangularloginApplication.java
@@ -0,0 +1,14 @@
+package dev.springangularlogin.springangularlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringangularloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringangularloginApplication.class, args);
+		System.out.println("Starting server...");
+	}
+
+}
---
+++ backend/springangularlogin/src/test/java/dev/springangularlogin/springangularlogin/SpringangularloginApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.springangularlogin.springangularlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringangularloginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
